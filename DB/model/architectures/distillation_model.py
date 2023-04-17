e0f01c5a0bb76f78a3627f1b2e244c83ae9d55ce
zhangyingying520 <1530177403@qq.com>
2023-04-17 09:14:08 +0800

    “first”

---
+++ DB/model/architectures/distillation_model.py
@@ -0,0 +1,43 @@
+from paddle import nn
+from modeling.transforms import build_transform
+from modeling.backbones import build_backbone
+from modeling.necks import build_neck
+from modeling.heads import build_head
+from .base_model import BaseModel
+from utils.save_load import load_pretrained_params
+
+__all__ = ['DistillationModel']
+
+
+class DistillationModel(nn.Layer):
+    def __init__(self, config):
+        """
+        the module for OCR distillation.
+        args:
+            config (dict): the super parameters for module.
+        """
+        super().__init__()
+        self.model_list = []
+        self.model_name_list = []
+        for key in config["Models"]:
+            model_config = config["Models"][key]
+            freeze_params = False
+            pretrained = None
+            if "freeze_params" in model_config:
+                freeze_params = model_config.pop("freeze_params")
+            if "pretrained" in model_config:
+                pretrained = model_config.pop("pretrained")
+            model = BaseModel(model_config)
+            if pretrained is not None:
+                load_pretrained_params(model, pretrained)
+            if freeze_params:
+                for param in model.parameters():
+                    param.trainable = False
+            self.model_list.append(self.add_sublayer(key, model))
+            self.model_name_list.append(key)
+
+    def forward(self, x, data=None):
+        result_dict = dict()
+        for idx, model_name in enumerate(self.model_name_list):
+            result_dict[model_name] = self.model_list[idx](x, data)
+        return result_dict
